79be0a6fcf872db95a7e7dd9573a1d336118e8c0
tanjimash <tanjimashraf.cit.bd@gmail.com>
2022-04-13 10:53:47 +0600

    implement recursive function in binary search & RecursionError handled

--- Binary Search/binary_search_recursive.py
+++ Binary Search/binary_search_recursive.py
@@ -1,5 +1,5 @@
 # assigning binary search in recussive
-def binary_seach_recusive(number_list, target_value, left_index=0, right_index=0):
+def binary_search_recusive(number_list, target_value, left_index=0, right_index=0):
     # left_index = 0
     # right_index = len(number_list) - 1
     # mid_index = 0
@@ -13,21 +13,32 @@ def binary_seach_recusive(number_list, target_value, left_index=0, right_index=0
         return mid_index
 
     if mid_number < target_value:
-        # reset the left index of the list
-        return binary_seach_recusive(left_index=mid_index+1, right_index = len(number_list) - 1)
+        # handling 'RecursionError' (2nd phase), if out of range
+        if mid_index >= len(number_list)-1:
+            return -1
+
+
+
+        # reset the left index of the list to the (mid_index-1) & the right_index will be the (len(number_list) - 1)
+        # print('Mid Number (mid_number < target_value): ', mid_number)
+        right_index = len(number_list) - 1
+        return binary_search_recusive(number_list, target_value, left_index=mid_index+1, right_index=right_index)
     
     if mid_number > target_value:
-        # reset the right index of the list
-        return binary_seach_recusive(right_index=mid_index-1, right_index = len(number_list) - 1)
+        # handling 'RecursionError' (2nd phase), if out of range
+        if mid_index <= left_index:
+            return -1
+
+        # reset the right_index of the list to the (mid_index-1) & the left_index will be 0
+        print('Mid Number: ', mid_number)
+        return binary_search_recusive(number_list, target_value, left_index=0, right_index=mid_index-1)
         pass
-    pass
-
-
+    
 
 number_list = [12,15,17,19,21,24,45,67]
 
-target_num = 45
+target_num = 15
 
-result_bns_recurssion = binary_seach_recusive(number_list=number_list, target_value=target_num)
+result_bns_recurssion = binary_search_recusive(number_list=number_list, target_value=target_num)
 print(f'The targeted value is in the index (Binary Search): {result_bns_recurssion}')
 
