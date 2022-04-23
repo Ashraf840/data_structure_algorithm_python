ffdc3c0b3c663c3938f3a3b6be7cca57ded47302
tanjimash <tanjimashraf.cit.bd@gmail.com>
2022-04-23 10:08:57 +0600

    Bug Fix (Binary Search using Recurssion): If a targeted value doesn't exist in the list

--- Binary Search/binary_search_recursive.py
+++ Binary Search/binary_search_recursive.py
@@ -13,11 +13,9 @@ def binary_search_recusive(number_list, target_value, left_index=0, right_index=
         return mid_index
 
     if mid_number < target_value:
-        # handling 'RecursionError' (2nd phase), if out of range
+        # handling 'RecursionError' (2nd phase), if out of range or value doesn't exist
         if mid_index >= len(number_list)-1:
             return -1
-
-
 
         # reset the left index of the list to the (mid_index-1) & the right_index will be the (len(number_list) - 1)
         # print('Mid Number (mid_number < target_value): ', mid_number)
@@ -25,20 +23,25 @@ def binary_search_recusive(number_list, target_value, left_index=0, right_index=
         return binary_search_recusive(number_list, target_value, left_index=mid_index+1, right_index=right_index)
     
     if mid_number > target_value:
-        # handling 'RecursionError' (2nd phase), if out of range
-        if mid_index <= left_index:
+        # handling 'RecursionError' (2nd phase), if out of range or value doesn't exist
+        if mid_index <= len(number_list)-1:
             return -1
 
         # reset the right_index of the list to the (mid_index-1) & the left_index will be 0
         print('Mid Number: ', mid_number)
         return binary_search_recusive(number_list, target_value, left_index=0, right_index=mid_index-1)
-        pass
-    
+
 
 number_list = [12,15,17,19,21,24,45,67]
 
-target_num = 15
+target_num = 45
 
 result_bns_recurssion = binary_search_recusive(number_list=number_list, target_value=target_num)
 print(f'The targeted value is in the index (Binary Search): {result_bns_recurssion}')
 
+
+
+# [Cons]
+# This application is not capable of handling the unsorted elements in the list.
+
+# [Note]: Time complexity is not measured. Need to implement in 100k elems
