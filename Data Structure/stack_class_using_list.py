768a7fc9b7d8a3ebab5b39319d4bb2719ae0fdda
ashraf840 <shift2unleashed2011@gmail.com>
2022-12-31 18:03:26 +0600

    Stack class: make attributes private

--- Data Structure/stack_class_using_list.py
+++ Data Structure/stack_class_using_list.py
@@ -5,7 +5,7 @@ class Stack(object):
         Use defined stack data structure using native list in Python. 
         It follows the LIFO (Last In/First Out)/FILO (First In/Last Out) order.
         """
-        self.items = []
+        self.__items = []
     
     def push(self, item=None):
         """
@@ -13,7 +13,7 @@ class Stack(object):
         return: Void/None
         """
         if item != None:
-            self.items.append(item)
+            self.__items.append(item)
         else:
             # return None
             raise TypeError("1 positional argument is missing: 'item'")
@@ -23,8 +23,8 @@ class Stack(object):
         Remove an element from the last index of the stack. 
         If no element is available to remove, return None.
         """
-        if len(self.items):
-            self.items.pop()
+        if len(self.__items):
+            self.__items.pop()
         else:
             return None
     
@@ -33,8 +33,8 @@ class Stack(object):
         View the size/length of the stack.
         return: integer/size
         """
-        if len(self.items):
-            return len(self.items)
+        if len(self.__items):
+            return len(self.__items)
         else:
             return None
     
@@ -43,7 +43,7 @@ class Stack(object):
         View if the stack is empty or not.
         return: boolean
         """
-        if self.items != []:
+        if self.__items != []:
             return False
         else:
             return True
@@ -55,8 +55,8 @@ class Stack(object):
         If the stack is empty, return None.
         return: elemtent/None
         """
-        if len(self.items):
-            return self.items[index]
+        if len(self.__items):
+            return self.__items[index]
         else:
             return None
 
