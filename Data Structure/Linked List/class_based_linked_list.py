b28be3b62e8afa676e55ac21ccf545490c84adda
robindas <robin.das@doer.com.bd>
2023-11-23 22:28:53 +0600

    Linked List: build a method to add node at the beginning

--- Data Structure/Linked List/class_based_linked_list.py
+++ Data Structure/Linked List/class_based_linked_list.py
@@ -15,8 +15,19 @@ class LinkedList:
     
     def add_begin(self, data):
         new_node = Node(data)
-        new_node.ref = self.head    # If prev created node is existed, then assign the object's mem-location as the new_node's ref attr.
-        self.head = new_node        # Since the node will be added at the beginning, assign the head ref with the newly created node object.
+        new_node.ref = self.head    # If previously created node is existed, then assign the object's mem-location as the new_node's ref attr. Basically, we can check using the if-condition whether the linked list is empty ot not, then in the empty block, the new_node would only be assigned to the self.head & in the non-empty block firstly assing the self.head obj to new.node's ref, then assign the new_node to "self.head".
+        self.head = new_node        # Since the node will be added at the beginning, assign the head with the newly created node object.
+    
+    def add_end(self, data):
+        new_node = Node(data)
+        n = self.head
+        if n is None:
+            self.head = new_node
+        else:
+            while n.ref is not None:
+                n = n.ref
+            n.ref = new_node
+
     
     def print_ll(self):
         n = self.head
@@ -31,4 +42,5 @@ ll = LinkedList()
 ll.add_begin(10)
 ll.add_begin(20)
 ll.add_begin(30)    # The lastly added node will be the first node of the LL.
+ll.add_end(40)
 ll.print_ll()
