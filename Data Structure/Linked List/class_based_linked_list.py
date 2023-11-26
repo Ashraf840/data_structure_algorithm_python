d6597df14235b3ac1576c6e06a770967003a6920
robindas <robin.das@doer.com.bd>
2023-11-26 10:33:56 +0600

    Linked List: Add before node

--- Data Structure/Linked List/class_based_linked_list.py
+++ Data Structure/Linked List/class_based_linked_list.py
@@ -18,6 +18,7 @@ class LinkedList:
         new_node.ref = self.head    # If previously created node is existed, then assign the object's mem-location as the new_node's ref attr. Basically, we can check using the if-condition whether the linked list is empty ot not, then in the empty block, the new_node would only be assigned to the self.head & in the non-empty block firstly assing the self.head obj to new.node's ref, then assign the new_node to "self.head".
         self.head = new_node        # Since the node will be added at the beginning, assign the head with the newly created node object.
     
+
     def add_end(self, data):
         new_node = Node(data)
         n = self.head
@@ -28,10 +29,11 @@ class LinkedList:
                 n = n.ref
             n.ref = new_node
 
+
     def add_after_node(self, data, x):
         n = self.head
         # Check for specific value in the array of linked nodes; it'll bring two conculsions: LL is empty or found the specific node
-        while n is not None:
+        while n is not None:    # This while-loop will check for: if LL is empty & specified node doesn't exist in the LL.
             if n.data == x:
                 break
             n = n.ref
@@ -43,19 +45,51 @@ class LinkedList:
             new_node.ref = n.ref
             n.ref = new_node
     
+
+    def add_before_node(self, data, x):
+        n = self.head
+        # Check if LL is empty
+        if n is None:
+            print("Linked list is empty!")  # Since the method signifies adding node before specific node, this method is unable serve it's purpose while the LL is empty
+            return
+        else:
+            # Check if the specific ndoe is the first node, the apply the "add_begin()" method
+            if n.data == x:
+                new_node = Node(data)
+                new_node.ref = n
+                self.head = new_node
+                return
+            else:
+                # For rest of the nodes in the LL, traverse the entire nodes of the LL
+                while n.ref is not None:    # Looping with n.ref != Node, since if the specific node doesn't exist in the LL, the while-loop will traverse till the end, which will ultimately assign None to n variable, since a Nonetype variable cannot contain data variable, it will throw an error.
+                    # Inside this loop, check the next node's data of the current comparing the given data to put the n variable with the state of previous node of the given node
+                    if n.ref.data == x:
+                        break
+                    n = n.ref
+                if n.ref is None:          # emplain why use n.ref instead of n only?
+                    print("Node is not present in the linked list")
+                else:
+                    new_node = Node(data)
+                    new_node.ref = n.ref
+                    n.ref = new_node
+    
+
     def print_ll(self):
         n = self.head
         if n is None:
             print("Linked list is empty!")
         else:
             while n is not None:
-                print(n.data)
+                print(f"{n.data} --->", end=" ")    # Better visual representation
                 n = n.ref
+            print()
+
 
 ll = LinkedList()
-ll.add_begin(10)
-ll.add_begin(20)
-ll.add_begin(30)    # The lastly added node will be the first node of the LL.
 ll.add_end(40)
-ll.add_after_node(50, 40)
+ll.add_begin(30)    # The lastly added node will be the first node of the LL.
+ll.add_begin(20)
+ll.add_begin(10)
+ll.add_after_node(60, 40)
+ll.add_before_node(50, 70)
 ll.print_ll()
