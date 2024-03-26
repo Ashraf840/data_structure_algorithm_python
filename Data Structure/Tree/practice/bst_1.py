f6fccf4b122825591c0dd4aca189f4901f6fdb4b
Ashraf840 <shift2unleashed2011@gmail.com>
2024-03-27 00:36:57 +0600

    Practice BST: Tree node insertion & traversal (inorder)

---
+++ Data Structure/Tree/practice/bst_1.py
@@ -0,0 +1,38 @@
+class Node:
+    def __init__(self, data):
+        self.left = None
+        self.data = data
+        self.right = None
+
+class Tree:
+    def createNode(self, data):
+        return Node(data)
+    
+    def insert(self, node, data):
+        if node is None:
+            return self.createNode(data)
+        if data < node.data:
+            node.left=self.insert(node.left, data)
+        else:
+            node.right=self.insert(node.right, data)
+        return node     # Return the child nodes to their subsequent parents after populating left & right side of the node
+
+    def traverse_inorder(self, node):
+        if node is not None:
+            self.traverse_inorder(node.left)
+            print(node.data)
+            self.traverse_inorder(node.right)
+
+bst = Tree()
+node = bst.createNode(5)
+bst.insert(node, 2)
+bst.insert(node, 10)
+bst.insert(node, 7)
+bst.insert(node, 15)
+bst.insert(node, 12)
+bst.insert(node, 20)
+bst.insert(node, 30)
+bst.insert(node, 6)
+bst.insert(node, 8)
+
+bst.traverse_inorder(node)
